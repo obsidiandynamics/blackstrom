7158ff51c2810ba92967ee55ea7da1e084652526
Emil Koutanov <ekoutanov@gmail.com>
2018-01-03 21:27:20 +1100

    Added docker stack

--- kafka/src/test/java/com/obsidiandynamics/blackstrom/kafka/RunKafkaConsumer10.java
+++ kafka/src/test/java/com/obsidiandynamics/blackstrom/kafka/RunKafkaConsumer10.java
@@ -7,24 +7,32 @@ import org.apache.kafka.common.serialization.*;
 
 public final class RunKafkaConsumer10 {
   public static void main(String[] args) {
-    final Properties props = new Properties();
-    props.put("bootstrap.servers", "10.134.32.26:9092");
-    props.put("group.id", "sample");
-    props.put("key.deserializer", StringDeserializer.class.getName());
-    props.put("value.deserializer", StringDeserializer.class.getName());
+    final Properties props = new PropertiesBuilder()
+        .with("bootstrap.servers", "localhost:9092")
+        .with("group.id", "sample")
+        .with("key.deserializer", StringDeserializer.class.getName())
+        .with("value.deserializer", StringDeserializer.class.getName())
+        .build();
     final KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props); 
-    consumer.subscribe(Arrays.asList("platform.push"));
+    consumer.subscribe(Arrays.asList("test"));
+    
     try {
       while (true) {
         System.out.println("polling...");
         final ConsumerRecords<String, String> records = consumer.poll(1_000);
-        System.out.println("got " + records + " records");
+        System.out.format("got %,d records\n", list(records).size());
         for (ConsumerRecord<String, String> record : records) {
-          System.out.println(record.offset() + ": " + record.value());
+          System.out.format("%,d: %s\n", record.offset(), record.value());
         }
       }
     } finally {
       consumer.close();
     }
+  }
+  
+  private static List<ConsumerRecord<String, String>> list(ConsumerRecords<String, String> records) {
+    final List<ConsumerRecord<String, String>> list = new ArrayList<>();
+    records.iterator().forEachRemaining(list::add);
+    return list;
   }
 }
