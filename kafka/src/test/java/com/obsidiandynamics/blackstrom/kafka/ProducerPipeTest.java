3ee2bd32d5bf95863e7d5a1549fa3f9205a56048
Emil Koutanov <ekoutanov@gmail.com>
2018-02-09 20:04:12 +1100

    Consumer/ProducerPipe tests

--- kafka/src/test/java/com/obsidiandynamics/blackstrom/kafka/ProducerPipeTest.java
+++ kafka/src/test/java/com/obsidiandynamics/blackstrom/kafka/ProducerPipeTest.java
@@ -1,41 +1,97 @@
 package com.obsidiandynamics.blackstrom.kafka;
 
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 import java.util.*;
 
+import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.clients.producer.*;
-import org.apache.kafka.common.serialization.*;
 import org.junit.*;
 import org.slf4j.*;
 
-import com.obsidiandynamics.blackstrom.ledger.*;
+import com.obsidiandynamics.await.*;
 import com.obsidiandynamics.blackstrom.model.*;
+import com.obsidiandynamics.blackstrom.util.*;
 import com.obsidiandynamics.indigo.util.*;
 
 public final class ProducerPipeTest {
+  private final Timesert wait = Wait.SHORT;
+  
+  private ProducerPipe<String, Message> pipe;
+  
+  @After
+  public void after() {
+    if (pipe != null) pipe.terminate().joinQuietly();
+  }
+  
   @Test
   public void testSendDisposedAsync() {
     final Logger log = mock(Logger.class);
     final Kafka<String, Message> kafka = new MockKafka<>();
-    final Properties props = new PropertiesBuilder()
-        .with("key.serializer", StringSerializer.class.getName())
-        .with("value.serializer", KafkaMessageSerializer.class.getName())
-        .build();
-    final Producer<String, Message> producer = kafka.getProducer(props);
-    final ProducerPipe<String, Message> pp = 
-        new ProducerPipe<>(new ProducerPipeConfig().withAsync(true), producer, ProducerPipe.class.getSimpleName(), log);
+    final Producer<String, Message> producer = kafka.getProducer(TestProps.producer());
+    pipe = new ProducerPipe<>(new ProducerPipeConfig().withAsync(true), producer, ProducerPipe.class.getSimpleName(), log);
 
-    try {
-      pp.closeProducer();
-      final Proposal proposal = new Proposal("B100", new String[0], null, 0);
-      final ProducerRecord<String, Message> rec = new  ProducerRecord<>("test", proposal);
-      pp.send(rec, null);
-      
-      TestSupport.sleep(10);
-      verifyNoMoreInteractions(log);
-    } finally {
-      pp.terminate().joinQuietly();
-    }
+    pipe.closeProducer();
+    final Proposal proposal = new Proposal("B100", new String[0], null, 0);
+    final ProducerRecord<String, Message> rec = new  ProducerRecord<>("test", proposal);
+    pipe.send(rec, null);
+    
+    TestSupport.sleep(10);
+    verifyNoMoreInteractions(log);
+  }
+  
+  @Test
+  public void testSendAsync() {
+    final Logger log = mock(Logger.class);
+    final Kafka<String, Message> kafka = new MockKafka<>();
+    final Producer<String, Message> producer = kafka.getProducer(TestProps.producer());
+    pipe = new ProducerPipe<>(new ProducerPipeConfig().withAsync(true), producer, ProducerPipe.class.getSimpleName(), log);
+
+    final Consumer<String, Message> consumer = kafka.getConsumer(TestProps.consumer());
+    consumer.subscribe(Arrays.asList("test"));
+    final Proposal proposal = new Proposal("B100", new String[0], null, 0);
+    final ProducerRecord<String, Message> rec = new  ProducerRecord<>("test", proposal);
+    pipe.send(rec, null);
+    
+    wait.until(() -> {
+      assertEquals(1, consumer.poll(1).count());
+    });
+  }
+  
+  @Test
+  public void testSendSync() {
+    final Logger log = mock(Logger.class);
+    final Kafka<String, Message> kafka = new MockKafka<>();
+    final Producer<String, Message> producer = kafka.getProducer(TestProps.producer());
+    pipe = new ProducerPipe<>(new ProducerPipeConfig().withAsync(false), producer, ProducerPipe.class.getSimpleName(), log);
+
+    final Consumer<String, Message> consumer = kafka.getConsumer(TestProps.consumer());
+    consumer.subscribe(Arrays.asList("test"));
+    final Proposal proposal = new Proposal("B100", new String[0], null, 0);
+    final ProducerRecord<String, Message> rec = new  ProducerRecord<>("test", proposal);
+    pipe.send(rec, null);
+    
+    wait.until(() -> {
+      assertEquals(1, consumer.poll(1).count());
+    });
+  }
+  
+  @Test
+  public void testSendError() {
+    final Logger log = mock(Logger.class);
+    final Kafka<String, Message> kafka = new MockKafka<String, Message>()
+        .withSendRuntimeExceptionGenerator(r -> new RuntimeException("testSendError"));
+    final Producer<String, Message> producer = kafka.getProducer(TestProps.producer());
+    pipe = new ProducerPipe<>(new ProducerPipeConfig().withAsync(false), producer, ProducerPipe.class.getSimpleName(), log);
+
+    final Proposal proposal = new Proposal("B100", new String[0], null, 0);
+    final ProducerRecord<String, Message> rec = new  ProducerRecord<>("test", proposal);
+    pipe.send(rec, null);
+    
+    wait.until(() -> {
+      verify(log).error(any(), (Throwable) any());
+    });
   }
 }
