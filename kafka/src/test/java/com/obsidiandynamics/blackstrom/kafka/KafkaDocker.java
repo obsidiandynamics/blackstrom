613d7cf29f8ff9d4491def89d36545b3bb2257df
Emil Koutanov <ekoutanov@gmail.com>
2018-02-15 13:22:54 +1100

    Waiting for broker to come up

--- kafka/src/test/java/com/obsidiandynamics/blackstrom/kafka/KafkaDocker.java
+++ kafka/src/test/java/com/obsidiandynamics/blackstrom/kafka/KafkaDocker.java
@@ -3,6 +3,7 @@ package com.obsidiandynamics.blackstrom.kafka;
 import java.io.*;
 import java.net.*;
 
+import com.obsidiandynamics.await.*;
 import com.obsidiandynamics.dockercompose.*;
 import com.obsidiandynamics.indigo.util.*;
 import com.obsidiandynamics.shell.*;
@@ -10,7 +11,7 @@ import com.obsidiandynamics.shell.*;
 public final class KafkaDocker {
   private static final String PROJECT = "blackstrom";
   private static final String PATH = "/usr/local/bin";
-  private static final int GRACE_PERIOD_MILLIS = 10_000;
+  private static final int BROKER_AWAIT_MILLIS = 600_000;
   
   private static final DockerCompose COMPOSE = new DockerCompose()
       .withShell(new BourneShell().withPath(PATH))
@@ -41,9 +42,14 @@ public final class KafkaDocker {
     }
 
     COMPOSE.checkInstalled();
-    final long took = TestSupport.tookThrowing(COMPOSE::up);
-    TestSupport.LOG_STREAM.format("took %,d ms (will wait a further %,d ms)\n", took, GRACE_PERIOD_MILLIS);
-    TestSupport.sleep(GRACE_PERIOD_MILLIS);
+    final long tookStart = TestSupport.tookThrowing(COMPOSE::up);
+    TestSupport.LOG_STREAM.format("took %,d ms (now waiting for broker to come up...)\n", tookStart);
+    final long tookAwait = awaitBroker();
+    TestSupport.LOG_STREAM.format("broker up, waited %,d ms\n", tookAwait);
+  }
+  
+  private static long awaitBroker() {
+    return TestSupport.took(() -> Timesert.wait(BROKER_AWAIT_MILLIS).untilTrue(KafkaDocker::isRunning));
   }
   
   public static void stop() throws Exception {
