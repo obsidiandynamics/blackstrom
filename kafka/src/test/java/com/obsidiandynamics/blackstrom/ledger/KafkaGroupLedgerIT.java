76111aceae9fbbaeb8ef257285d4e07726f659a5
Emil Koutanov <ekoutanov@gmail.com>
2018-02-19 09:13:53 +1100

    Random ledger naming

--- kafka/src/test/java/com/obsidiandynamics/blackstrom/ledger/KafkaGroupLedgerIT.java
+++ kafka/src/test/java/com/obsidiandynamics/blackstrom/ledger/KafkaGroupLedgerIT.java
@@ -1,14 +1,25 @@
 package com.obsidiandynamics.blackstrom.ledger;
 
+import java.util.*;
+
 import org.junit.*;
+import org.junit.runner.*;
+import org.junit.runners.*;
 
 import com.obsidiandynamics.await.*;
 import com.obsidiandynamics.blackstrom.codec.*;
 import com.obsidiandynamics.blackstrom.kafka.*;
 import com.obsidiandynamics.blackstrom.model.*;
 import com.obsidiandynamics.blackstrom.util.*;
+import com.obsidiandynamics.junit.*;
 
+@RunWith(Parameterized.class)
 public final class KafkaGroupLedgerIT extends AbstractGroupLedgerTest {
+  @Parameterized.Parameters
+  public static List<Object[]> data() {
+    return TestCycle.timesQuietly(1000);
+  }
+  
   @BeforeClass
   public static void beforeClass() throws Exception {
     KafkaDocker.start();
@@ -23,7 +34,7 @@ public final class KafkaGroupLedgerIT extends AbstractGroupLedgerTest {
   protected Ledger createLedger() {
     final Kafka<String, Message> kafka = 
         new KafkaCluster<>(new KafkaClusterConfig().withBootstrapServers("localhost:9092"));
-    final String topic = TestTopic.of(KafkaGroupLedgerIT.class, "json", JacksonMessageCodec.ENCODING_VERSION);
+    final String topic = TestTopic.of(KafkaGroupLedgerIT.class, "json-" + System.currentTimeMillis(), JacksonMessageCodec.ENCODING_VERSION);
     return new KafkaLedger(new KafkaLedgerConfig()
                            .withKafka(kafka)
                            .withTopic(topic)
