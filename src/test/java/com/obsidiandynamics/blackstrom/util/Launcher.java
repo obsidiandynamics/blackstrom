c894cc6c6ee72d481bacc4a707b2c88cb934648c
Emil Koutanov <ekoutanov@gmail.com>
2018-02-19 19:42:33 +1100

    Renamed Runner->Launcher

--- src/test/java/com/obsidiandynamics/blackstrom/util/Launcher.java
+++ src/test/java/com/obsidiandynamics/blackstrom/util/Launcher.java
@@ -6,42 +6,58 @@ import java.util.*;
 
 import com.obsidiandynamics.indigo.util.*;
 
-public final class Runner {
-  public static void main(String[] args) throws IOException {
-    final int packageCompressLevel = PropertyUtils.get("runner.packageCompressLevel", Integer::parseInt, 0);
+public final class Launcher {
+  public static void main(String[] args) {
+    try {
+      run(args);
+    } catch (Exception e) {
+      System.err.format("Error: %s\n", e);
+      e.printStackTrace();
+    }
+  }
+  
+  private static void run(String[] classes) throws Exception {
+    final int packageCompressLevel = PropertyUtils.get("launcher.packageCompressLevel", Integer::parseInt, 0);
     
-    Arrays.sort(args);
-    final int selection = PropertyUtils.get("runner.selection", Integer::parseInt, 0);
-    if (selection != 0) {
-      try {
-        run(args[selection - 1]);
-      } catch (Exception e) {
-        System.err.println("Error: " + e);
+    Arrays.sort(classes);
+    final String partialClassName = PropertyUtils.get("launcher.class", String::valueOf, null);
+    if (partialClassName != null) {
+      final String className = matchClass(partialClassName, classes);
+      if (className != null) {
+        run(className);
+      } else {
+        System.err.format("Error: could not match class '%s'\n", partialClassName);
       }
     } else {
       System.out.println("Select class to run");
-      for (int i = 0; i < args.length; i++) {
-        System.out.format("[%2d] %s\n", i + 1, formatClass(args[i], packageCompressLevel));
+      for (int i = 0; i < classes.length; i++) {
+        System.out.format("[%2d] %s\n", i + 1, formatClass(classes[i], packageCompressLevel));
       }
       
       try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
         final String read = reader.readLine();
         if (read != null && ! read.trim().isEmpty()) {
+          final int index;
           try {
-            final int index = Integer.parseInt(read.trim()) - 1;
-            try {
-              run(args[index]);
-            } catch (Exception e) {
-              System.err.println("Error: " + e);
-            }
+            index = Integer.parseInt(read.trim()) - 1;
           } catch (NumberFormatException e) {
-            System.err.println(e.getMessage());
+            System.err.format("Invalid selection '%s'\n", read.trim());
+            return;
           }
+          if (index < 0 || index >= classes.length) {
+            System.err.format("Invalid selection '%s'\n", read.trim());
+            return;
+          }
+          run(classes[index]);
         } else {
           System.out.format("Exiting\n");
         }
       }
     }
+  }
+  
+  private static String matchClass(String partialClassName, String[] classes) {
+    return Arrays.stream(classes).filter(c -> c.endsWith(partialClassName)).findAny().orElse(null);
   }
   
   private static CharSequence formatClass(String className, int packageCompressLevel) {
