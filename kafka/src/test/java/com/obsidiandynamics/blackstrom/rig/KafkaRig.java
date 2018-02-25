a5bcb7aa1042be2829a3809b574ea939431d3650
Emil Koutanov <ekoutanov@gmail.com>
2018-02-25 13:56:36 +1100

    Printing of common properties; configurable async prod/cons

--- kafka/src/test/java/com/obsidiandynamics/blackstrom/rig/KafkaRig.java
+++ kafka/src/test/java/com/obsidiandynamics/blackstrom/rig/KafkaRig.java
@@ -18,9 +18,12 @@ import com.obsidiandynamics.blackstrom.util.props.*;
 import com.obsidiandynamics.indigo.util.*;
 
 public final class KafkaRig {
-  private static final String clusterName = get("rig.cluster.name", String::valueOf, "rig");
+  private static final Properties common = new Properties();
   
-  private static final String bootstrapServers = get("bootstrap.servers", String::valueOf, "localhost:9092");
+  private static final String cluster = getOrSet(common, "rig.cluster", String::valueOf, "rig");
+  private static final String bootstrapServers = getOrSet(common, "bootstrap.servers", String::valueOf, "localhost:9092");
+  private static final boolean asyncProducer = getOrSet(common, "rig.async.producer", Boolean::valueOf, false);
+  private static final boolean asyncConsumer = getOrSet(common, "rig.async.consumer", Boolean::valueOf, true);
   
   private static final Logger log = LoggerFactory.getLogger(KafkaRig.class);
   
@@ -28,7 +31,7 @@ public final class KafkaRig {
       .withBootstrapServers(bootstrapServers);
 
   private static final String topic = 
-      TestTopic.of(KafkaRig.class, "kryo", KryoMessageCodec.ENCODING_VERSION, clusterName);
+      TestTopic.of(KafkaRig.class, "kryo", KryoMessageCodec.ENCODING_VERSION, cluster);
   
   private static void before() throws InterruptedException, ExecutionException, TimeoutException {
     try (KafkaAdmin admin = KafkaAdmin.forConfig(config, AdminClient::create)) {
@@ -43,6 +46,8 @@ public final class KafkaRig {
   
   private static Ledger createLedger() {
     return new KafkaLedger(new KafkaLedgerConfig()
+                           .withProducerPipeConfig(new ProducerPipeConfig().withAsync(asyncProducer))
+                           .withConsumerPipeConfig(new ConsumerPipeConfig().withAsync(asyncConsumer))
                            .withKafka(new KafkaCluster<>(config))
                            .withTopic(topic)
                            .withCodec(new KryoMessageCodec(true, new KryoBankExpansion()))
@@ -55,7 +60,7 @@ public final class KafkaRig {
   
   public static final class Initiator {
     public static void main(String[] args) throws Exception {
-      final Properties props = new Properties(System.getProperties());
+      final Properties props = new Properties(common);
       final long _runs = getOrSet(props, "rig.runs", Long::valueOf, 1_000_000L);
       final int _backlogTarget = getOrSet(props, "rig.backlog", Integer::valueOf, 10_000);
       final int cycles = getOrSet(props, "rig.cycles", Integer::valueOf, 1);
@@ -73,7 +78,7 @@ public final class KafkaRig {
           log = KafkaRig.log;
           ledgerFactory = KafkaRig::createLedger;
           channelFactory = KafkaRig::createChannel;
-          clusterName = KafkaRig.clusterName;
+          clusterName = KafkaRig.cluster;
           runs = _runs;
           backlogTarget = _backlogTarget;
         }}.create().run();
@@ -83,7 +88,7 @@ public final class KafkaRig {
   
   public static final class Cohort {
     public static void main(String[] args) throws Exception {
-      final Properties props = new Properties(System.getProperties());
+      final Properties props = new Properties(common);
       final String _branchId = getOrSet(props, "rig.branch.id", String::valueOf, null);
       assertNotNull("rig.branch.id not set", _branchId);
       printProps(props);
@@ -93,7 +98,7 @@ public final class KafkaRig {
         log = KafkaRig.log;
         ledgerFactory = KafkaRig::createLedger;
         channelFactory = KafkaRig::createChannel;
-        clusterName = KafkaRig.clusterName;
+        clusterName = KafkaRig.cluster;
         branchId = _branchId;
       }}.create();
       
@@ -109,7 +114,7 @@ public final class KafkaRig {
         log = KafkaRig.log;
         ledgerFactory = KafkaRig::createLedger;
         channelFactory = KafkaRig::createChannel;
-        clusterName = KafkaRig.clusterName;
+        clusterName = KafkaRig.cluster;
       }}.create();
       
       TestSupport.sleep(Integer.MAX_VALUE);
