1c04f0ecf73ec9b19207f47d23f8c57436d25601
Emil Koutanov <ekoutanov@gmail.com>
2018-03-20 16:42:49 +1100

    SubscriberGroupTest: reset cycles to x1

--- hazelcast/src/test/java/com/obsidiandynamics/blackstrom/hazelcast/queue/SubscriberGroupTest.java
+++ hazelcast/src/test/java/com/obsidiandynamics/blackstrom/hazelcast/queue/SubscriberGroupTest.java
@@ -19,7 +19,7 @@ import com.obsidiandynamics.junit.*;
 public final class SubscriberGroupTest extends AbstractPubSubTest {
   @Parameterized.Parameters
   public static List<Object[]> data() {
-    return TestCycle.timesQuietly(100); //TODO
+    return TestCycle.timesQuietly(1);
   }
   
   /**
