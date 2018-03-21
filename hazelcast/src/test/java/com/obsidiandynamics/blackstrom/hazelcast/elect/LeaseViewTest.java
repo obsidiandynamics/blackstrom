fe5192f6dd089803c76c708e4b640046276a7bc9
Emil Koutanov <ekoutanov@gmail.com>
2018-03-21 12:57:45 +1100

    LeaseViewImpl coverage

--- hazelcast/src/test/java/com/obsidiandynamics/blackstrom/hazelcast/elect/LeaseViewTest.java
+++ hazelcast/src/test/java/com/obsidiandynamics/blackstrom/hazelcast/elect/LeaseViewTest.java
@@ -20,6 +20,29 @@ public final class LeaseViewTest {
   }
   
   @Test
+  public void testCopy() {
+    final LeaseViewImpl orig = new LeaseViewImpl(0);
+    final Lease lease0 = Lease.forever(UUID.randomUUID());
+    orig.put("key0", lease0);
+    
+    final LeaseViewImpl copy = new LeaseViewImpl(orig, 1);
+    assertEquals(1, copy.getVersion());
+    assertEquals(Collections.singletonMap("key0", lease0), copy.asMap());
+    
+    // mutate the copy and ensure that the original remains unchanged
+    final Lease lease1 = Lease.forever(UUID.randomUUID());
+    copy.put("key1", lease1);
+    assertEquals(Collections.singletonMap("key0", lease0), orig.asMap());
+    
+    // mutate the original and ensure that the copy remains unchanged
+    final Lease lease2 = Lease.forever(UUID.randomUUID());
+    orig.put("key2", lease2);
+    assertEquals(2, copy.asMap().size());
+    assertEquals(lease0, copy.asMap().get("key0"));
+    assertEquals(lease1, copy.asMap().get("key1"));
+  }
+  
+  @Test
   public void testGetTenant() {
     final LeaseViewImpl v = new LeaseViewImpl(0);
     final UUID c = UUID.randomUUID();
