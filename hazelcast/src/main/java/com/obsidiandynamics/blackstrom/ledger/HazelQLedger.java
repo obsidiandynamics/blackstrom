299c2fd60d43ecfb933f5d960b335cef2dcf4bdb
Emil Koutanov <ekoutanov@gmail.com>
2018-03-21 21:21:39 +1100

    More work on HazelQLedger

--- hazelcast/src/main/java/com/obsidiandynamics/blackstrom/ledger/HazelQLedger.java
+++ hazelcast/src/main/java/com/obsidiandynamics/blackstrom/ledger/HazelQLedger.java
@@ -2,6 +2,7 @@ package com.obsidiandynamics.blackstrom.ledger;
 
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
 import java.util.stream.*;
 
 import org.slf4j.*;
@@ -9,6 +10,7 @@ import org.slf4j.*;
 import com.hazelcast.core.*;
 import com.obsidiandynamics.blackstrom.codec.*;
 import com.obsidiandynamics.blackstrom.handler.*;
+import com.obsidiandynamics.blackstrom.hazelcast.elect.*;
 import com.obsidiandynamics.blackstrom.hazelcast.queue.*;
 import com.obsidiandynamics.blackstrom.model.*;
 import com.obsidiandynamics.blackstrom.model.Message;
@@ -16,21 +18,33 @@ import com.obsidiandynamics.blackstrom.retention.*;
 import com.obsidiandynamics.blackstrom.worker.*;
 
 public final class HazelQLedger implements Ledger {
+  private final HazelcastInstance instance;
+  
   private final Logger log;
   
   private final StreamConfig streamConfig;
+  
+  private final ElectionConfig electionConfig;
   
   private final Publisher publisher;
   
   private final MessageCodec codec;
   
-//  private final List<ConsumerPipe<String, Message>> consumerPipes = new CopyOnWriteArrayList<>();
+  private final List<Subscriber> allSubscribers = new CopyOnWriteArrayList<>();
+  
+  private final Map<Integer, Subscriber> groupSubscribers = new ConcurrentHashMap<>();
+  
+  private final List<Receiver> receivers = new CopyOnWriteArrayList<>();
   
   private final List<ShardedFlow> flows = new CopyOnWriteArrayList<>(); 
+
+  private final AtomicInteger nextHandlerId = new AtomicInteger();
   
   public HazelQLedger(HazelcastInstance instance, HazelQLedgerConfig config) {
+    this.instance = instance;
     log = config.getLog();
     streamConfig = config.getStreamConfig();
+    electionConfig = config.getElectionConfig();
     codec = config.getCodec();
     final PublisherConfig pubConfig = new PublisherConfig()
         .withLog(log)
@@ -38,14 +52,45 @@ public final class HazelQLedger implements Ledger {
     publisher = Publisher.createDefault(instance, pubConfig);
   }
   
-  private void onRecord(Record record) {
-    //TODO
-  }
-
   @Override
   public void attach(MessageHandler handler) {
-    // TODO Auto-generated method stub
-    
+    final String group = handler.getGroupId();
+    final SubscriberConfig subConfig = new SubscriberConfig()
+        .withLog(log)
+        .withStreamConfig(streamConfig)
+        .withElectionConfig(electionConfig)
+        .withGroup(group);
+    final Subscriber subscriber = Subscriber.createDefault(instance, subConfig);
+    allSubscribers.add(subscriber);
+
+    final Integer handlerId;
+    final Retention retention;
+    if (group != null) {
+      handlerId = nextHandlerId.getAndIncrement();
+      groupSubscribers.put(handlerId, subscriber);
+      final ShardedFlow flow = new ShardedFlow();
+      retention = flow;
+      flows.add(flow);
+    } else {
+      handlerId = null;
+      retention = NopRetention.getInstance();
+    }
+
+    final MessageContext context = new DefaultMessageContext(this, handlerId, retention);
+    final Receiver receiver = subscriber.createReceiver(record -> {
+      final DefaultMessageId messageId = new DefaultMessageId(0, record.getOffset());
+      final Message message;
+      try {
+        message = codec.decode(record.getData());
+      } catch (Exception e) {
+        log.error(String.format("Could not decode message at offset %,d", record.getOffset()), e);
+        return;
+      }
+      message.setMessageId(messageId);
+      message.setShard(0);
+      handler.onMessage(context, message);
+    });
+    receivers.add(receiver);
   }
 
   @Override
@@ -59,14 +104,14 @@ public final class HazelQLedger implements Ledger {
     } catch (Exception e) {
       callback.onAppend(null, e);
     }
-    
   }
 
   @Override
   public void dispose() {
     final List<Terminable> terminables = new ArrayList<>();
     terminables.add(publisher);
-    //TODO add the subs and receivers
+    terminables.addAll(receivers);
+    terminables.addAll(allSubscribers);
     terminables.addAll(flows);
     final List<Joinable> joinables = terminables.stream().map(t -> t.terminate()).collect(Collectors.toList());
     joinables.forEach(j -> j.joinQuietly());
--- hazelcast/src/main/java/com/obsidiandynamics/blackstrom/ledger/HazelQLedgerConfig.java
+++ hazelcast/src/main/java/com/obsidiandynamics/blackstrom/ledger/HazelQLedgerConfig.java
@@ -3,14 +3,12 @@ package com.obsidiandynamics.blackstrom.ledger;
 import org.slf4j.*;
 
 import com.obsidiandynamics.blackstrom.codec.*;
+import com.obsidiandynamics.blackstrom.hazelcast.elect.*;
 import com.obsidiandynamics.blackstrom.hazelcast.queue.*;
 import com.obsidiandynamics.yconf.*;
 
 @Y
 public final class HazelQLedgerConfig {
-  @YInject
-  private String stream; 
-  
   @YInject
   private MessageCodec codec;
   
@@ -19,36 +17,43 @@ public final class HazelQLedgerConfig {
   
   @YInject
   private StreamConfig streamConfig = new StreamConfig();
-
-  String getStream() {
-    return stream;
-  }
-
-  public void setStream(String stream) {
-    this.stream = stream;
-  }
+  
+  @YInject
+  private ElectionConfig electionConfig = new ElectionConfig();
 
   MessageCodec getCodec() {
     return codec;
   }
 
-  public void setCodec(MessageCodec codec) {
+  public HazelQLedgerConfig withCodec(MessageCodec codec) {
     this.codec = codec;
+    return this;
   }
 
   Logger getLog() {
     return log;
   }
 
-  public void setLog(Logger log) {
+  public HazelQLedgerConfig withLog(Logger log) {
     this.log = log;
+    return this;
   }
 
   StreamConfig getStreamConfig() {
     return streamConfig;
   }
 
-  public void setStreamConfig(StreamConfig streamConfig) {
+  public HazelQLedgerConfig withStreamConfig(StreamConfig streamConfig) {
     this.streamConfig = streamConfig;
+    return this;
+  }
+  
+  ElectionConfig getElectionConfig() {
+    return electionConfig;
+  }
+  
+  public HazelQLedgerConfig withElectionConfig(ElectionConfig electionConfig) {
+    this.electionConfig = electionConfig;
+    return this;
   }
 }
