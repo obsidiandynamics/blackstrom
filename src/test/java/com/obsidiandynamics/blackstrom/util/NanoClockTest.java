7d4e74c39dbb86ee46d5ad4ef035f267c270cc5a
Emil Koutanov <ekoutanov@gmail.com>
2018-03-22 16:31:00 +1100

    Simplified NanoClock test

--- src/test/java/com/obsidiandynamics/blackstrom/util/NanoClockTest.java
+++ src/test/java/com/obsidiandynamics/blackstrom/util/NanoClockTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 import com.obsidiandynamics.assertion.*;
-import com.obsidiandynamics.indigo.util.*;
 
 public final class NanoClockTest {
   @Test
@@ -15,11 +14,6 @@ public final class NanoClockTest {
 
   @Test
   public void testTime() {
-    final long now = System.currentTimeMillis();
-    final long nanoNow = NanoClock.now();
-    assertTrue("nanoNow=" + nanoNow + ", now=" + now, nanoNow >= now * 1_000_000L);
-    TestSupport.sleep(1);
-    final long after = System.currentTimeMillis();
-    assertTrue("nanoNow=" + nanoNow + ", now=" + now + ", after=" + after, after * 1_000_000L >= nanoNow);
+    assertTrue(NanoClock.now() > 0);
   }
 }
