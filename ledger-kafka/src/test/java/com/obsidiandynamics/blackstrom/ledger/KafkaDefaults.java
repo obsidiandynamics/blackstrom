dc3611936829ec5fc018d8adbc220c0c69355df5
Emil Koutanov <ekoutanov@gmail.com>
2018-09-27 07:40:15 +1000

    Increased topic operation timeout

--- ledger-kafka/src/test/java/com/obsidiandynamics/blackstrom/ledger/KafkaDefaults.java
+++ ledger-kafka/src/test/java/com/obsidiandynamics/blackstrom/ledger/KafkaDefaults.java
@@ -2,6 +2,6 @@ package com.obsidiandynamics.blackstrom.ledger;
 
 public interface KafkaDefaults {
   static int CLUSTER_AWAIT = 120_000;
-  static int TOPIC_OPERATION = 10_000;
+  static int TOPIC_OPERATION = 30_000;
   static final String TOPIC_DATE_FORMAT = "yyyy-MM-dd-HHmmss";
 }
