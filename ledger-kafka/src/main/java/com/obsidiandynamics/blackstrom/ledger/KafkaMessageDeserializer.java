79f83bc601801e09701bb04212eea0386966e3e3
Emil Koutanov <ekoutanov@gmail.com>
2018-11-18 14:02:28 +1100

    Cleanup

--- ledger-kafka/src/main/java/com/obsidiandynamics/blackstrom/ledger/KafkaMessageDeserializer.java
+++ ledger-kafka/src/main/java/com/obsidiandynamics/blackstrom/ledger/KafkaMessageDeserializer.java
@@ -10,7 +10,6 @@ import org.apache.kafka.common.serialization.*;
 import com.obsidiandynamics.blackstrom.codec.*;
 import com.obsidiandynamics.blackstrom.model.*;
 import com.obsidiandynamics.format.*;
-import com.obsidiandynamics.func.*;
 import com.obsidiandynamics.zerolog.*;
 
 public final class KafkaMessageDeserializer implements Deserializer<Message> {
