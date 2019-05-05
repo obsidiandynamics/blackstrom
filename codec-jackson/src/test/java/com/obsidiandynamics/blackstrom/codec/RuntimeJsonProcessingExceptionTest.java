85f7a157e3dfb1d8245bec84a4b4e1daa081985f
Emil Koutanov <ekoutanov@gmail.com>
2019-05-05 20:44:55 +1000

    Exception constructor test

---
+++ codec-jackson/src/test/java/com/obsidiandynamics/blackstrom/codec/RuntimeJsonProcessingExceptionTest.java
@@ -0,0 +1,10 @@
+package com.obsidiandynamics.blackstrom.codec;
+
+import org.junit.*;
+
+public final class RuntimeJsonProcessingExceptionTest {
+  @Test
+  public void testConstructor() {
+    new RuntimeJsonProcessingException(null);
+  }
+}
